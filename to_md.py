6706e3da19a51a9681754bfbcbe636885b7daef3
Andrej730 <azhilenkov@gmail.com>
2026-02-23 13:09:49 +0500

    Refactor Python script for readibility

--- to_md.py
+++ to_md.py
@@ -6,11 +6,13 @@ To generate 's3data.json', use the following AWS CLI command (no credentials req
 
 """
 
-import functools
 import itertools
+import json
 import operator
 import re
 import sys
+from collections import defaultdict
+from typing import NamedTuple
 from urllib.parse import quote_plus
 
 import humanize
@@ -20,16 +22,24 @@ from packaging.utils import parse_wheel_filename
 PREFIX = "https://s3.amazonaws.com/ifcopenshell-builds/"
 
 
-def _():
-    import json
+class Asset(NamedTuple):
+    version: str
+    commit: str
+    last_modified: str
+    product: str
+    os: str
+    size: int
+    key: str
 
-    d = json.load(open(sys.argv[1]))
-    for c in d["Contents"]:
-        k = c["Key"]
-        if k.endswith(".zip"):
-            parts = k[:-4].rsplit("-", 3)
+
+def get_bucket_data():
+    bucket = json.load(open(sys.argv[1]))
+    for zip_data in bucket["Contents"]:
+        key: str = zip_data["Key"]
+        if key.endswith(".zip"):
+            parts = key.removesuffix(".zip").rsplit("-", 3)
             if len(parts) == 4:
-                product, version, hash, os = parts
+                product, version, commit, os = parts
 
                 if product in {"IfcConvert", "IfcGeomServer", "svgfill"}:
                     pass
@@ -38,14 +48,11 @@ def _():
                 else:
                     continue
 
-                if len(hash) == 7:
-                    pass
-                else:
+                if len(commit) != 7:
                     continue
 
-                if re.match(r"^v\d\.\d\.\d+$", version):
-                    pass
-                else:
+                # E.g. searching for 'v0.8.5'.
+                if not re.match(r"^v\d\.\d\.\d+$", version):
                     continue
 
                 if os in {"macosm164", "macos64", "linux64", "linuxarm64", "win32", "win64", "linux32"}:
@@ -53,9 +60,9 @@ def _():
                 else:
                     continue
 
-                yield version, hash, c["LastModified"], product, os, c["Size"], k
-        elif k.endswith(".whl"):
-            fixed = k.replace("ifcopenshell-python", "ifcopenshell_python")
+                yield Asset(version, commit, zip_data["LastModified"], product, os, zip_data["Size"], key)
+        elif key.endswith(".whl"):
+            fixed = key.replace("ifcopenshell-python", "ifcopenshell_python")
             fixed = re.sub(r"(v\d\.\d\.\d)(\-|\+)(\w{7})", "v0.8.1+\\3", fixed)
             try:
                 module_name, version, _, tags = parse_wheel_filename(fixed)
@@ -75,7 +82,16 @@ def _():
 
             abi = re.sub("c|p|y", "", tag.abi)
 
-            yield f"v{version.public}", version.local, c["LastModified"], f"{module_name}-{abi}", "WASM", c["Size"], k
+            assert version.local is not None
+            yield Asset(
+                f"v{version.public}",
+                version.local,
+                zip_data["LastModified"],
+                f"{module_name}-{abi}",
+                "WASM",
+                zip_data["Size"],
+                key,
+            )
 
 
 if len(sys.argv) != 2:
@@ -86,35 +102,55 @@ print("# IfcOpenShell Builds")
 print("Source code - [IfcOpenShell/build-listing](https://github.com/IfcOpenShell/build-listing)")
 print()
 
-hashtodate = dict(
-    (hash, functools.reduce(min, (t[1] for t in hash_dates)))
-    for hash, hash_dates in itertools.groupby(sorted((a[1], a[2]) for a in _()), key=operator.itemgetter(0))
-)
-data = natsort.natsorted(_(), reverse=True)
-for section, subsections in itertools.groupby(data, key=operator.itemgetter(0)):
+commit_dates: defaultdict[str, list[str]] = defaultdict(list)
+for asset in get_bucket_data():
+    commit_dates[asset.commit].append(asset.last_modified)
+hash_to_date = {commit: min(dates) for commit, dates in commit_dates.items()}
+
+data = natsort.natsorted(get_bucket_data(), reverse=True)
+
+for section, subsections in itertools.groupby(data, key=operator.attrgetter("version")):
     print("##", section)
 
-    for hash, rows in itertools.groupby(
-        sorted(subsections, key=lambda t: hashtodate[t[1]], reverse=True), key=operator.itemgetter(1)
+    for commit, rows in itertools.groupby(
+        sorted(subsections, key=lambda t: hash_to_date[t.commit], reverse=True),
+        key=operator.attrgetter("commit"),
     ):
-        print("###", hash, "(%s)" % hashtodate[hash])
+        print(f"### {commit} ({hash_to_date[commit]})")
         rows = list(rows)
-        product, os, size = map(lambda vs: natsort.natsorted(set(vs)), zip(*(r[3:6] for r in rows)))
-        osh = list(
-            map(
-                lambda s: s[0].upper() + s[1:],
-                map(
-                    lambda s: re.sub(r"(32|64)", " \\1bit", s.replace("m1", " M1").replace("arm", " ARM")).replace(
-                        "os", "OS"
-                    ),
-                    os,
-                ),
-            )
-        )
-        d = dict((r[3:5], (humanize.naturalsize(r[5]), quote_plus(r[6]))) for r in rows)
+
+        def unique_sorted(vs: list[str]) -> list[str]:
+            return natsort.natsorted(set(vs))
+
+        product = unique_sorted([r.product for r in rows])
+        os = unique_sorted([r.os for r in rows])
+
+        def format_os(os: str) -> str:
+            os = os.replace("m1", " M1")
+            os = os.replace("arm", " ARM")
+            os = os.replace("os", "OS")
+            os = re.sub(r"(32|64)", r" \1bit", os)
+            os = f"{os[0].upper()}{os[1:]}"
+            return os
+
+        # Header.
+        osh = list(map(format_os, os))
+        existing_products = {(r.product, r.os): (humanize.naturalsize(r.size), quote_plus(r.key)) for r in rows}
         print()
         print("item|", "|".join(osh))
-        print("|".join(["---"] * (len(osh) + 1)))
-        for p in product:
-            print(p, "|", "|".join(map(lambda t: "[%%s](%s%%s)" % PREFIX % t, (d.get((p, o), ("-", "")) for o in os))))
+        print("|".join(["---"] + [":---:"] * len(osh)))
+
+        # Rows.
+        for product_ in product:
+            links: list[str] = []
+            for os_ in os:
+                size, filename = existing_products.get((product_, os_), ("-", ""))
+                if filename:
+                    url = f"{PREFIX}{filename}"
+                    link = f"[{size}]({url})"
+                else:
+                    link = "-"
+                links.append(link)
+
+            print(product_, "|", "|".join(links))
         print()
